609950aeaec0d4506827b1b9b7350af141b40984
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2022-12-16 18:55:26 -0600

    ad profiles

--- macros/get_profile_columns.sql
+++ macros/get_profile_columns.sql
@@ -7,12 +7,12 @@
     {"name": "account_name", "datatype": dbt.type_string()},
     {"name": "account_sub_type", "datatype": dbt.type_string()},
     {"name": "account_type", "datatype": dbt.type_string()},
-    {"name": "account_valid_payment_method", "datatype": dbt.type_bool()},
+    {"name": "account_valid_payment_method", "datatype": dbt.type_boolean()},
     {"name": "country_code", "datatype": dbt.type_string()},
-    {"name": "currency_code", "datatype": dbt.type_string()}
+    {"name": "currency_code", "datatype": dbt.type_string()},
     {"name": "daily_budget", "datatype": dbt.type_int()},
     {"name": "timezone", "datatype": dbt.type_string()},
-    {"name": "_fivetran_deleted", "datatype": dbt.type_bool()}
+    {"name": "_fivetran_deleted", "datatype": dbt.type_boolean()}
 ] %}
 
 {{ return(columns) }}
