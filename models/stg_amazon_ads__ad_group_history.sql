53bf8aaded9401408f952d02546066831872293f
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-08-22 09:54:06 -0500

    MagicBot/add-union-schema updates

--- models/stg_amazon_ads__ad_group_history.sql
+++ models/stg_amazon_ads__ad_group_history.sql
@@ -1,3 +1,5 @@
+ADD source_relation WHERE NEEDED + CHECK JOINS AND WINDOW FUNCTIONS! (Delete this line when done.)
+
 {{ config(enabled=var('ad_reporting__amazon_ads_enabled', True)) }}
 
 with base as (
@@ -15,12 +17,19 @@ fields as (
                 staging_columns=get_ad_group_history_columns()
             )
         }}
+    
+        {{ fivetran_utils.source_relation(
+            union_schema_variable='amazon_ads_union_schemas', 
+            union_database_variable='amazon_ads_union_databases') 
+        }}
+
     from base
 ),
 
 final as (
-    
-    select 
+
+    select
+        source_relation, 
         cast(id as {{ dbt.type_string() }}) as ad_group_id,
         cast(campaign_id as {{ dbt.type_string() }}) as campaign_id,
         creation_date,
@@ -29,7 +38,7 @@ final as (
         name as ad_group_name,
         serving_status,
         state,
-        row_number() over (partition by id order by last_updated_date desc) = 1 as is_most_recent_record
+        row_number() over (partition by source_relation, id order by last_updated_date desc) = 1 as is_most_recent_record
     from fields
 )
 
