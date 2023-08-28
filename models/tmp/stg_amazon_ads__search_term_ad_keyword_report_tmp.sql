1f762a96a649c61214a7994a0cd4789e5801601c
fivetran-catfritz <111930712+fivetran-catfritz@users.noreply.github.com>
2023-08-28 16:18:40 -0500

    MagicBot/add-union-schema updates

--- models/tmp/stg_amazon_ads__search_term_ad_keyword_report_tmp.sql
+++ models/tmp/stg_amazon_ads__search_term_ad_keyword_report_tmp.sql
@@ -1,4 +1,12 @@
-{{ config(enabled=var('ad_reporting__amazon_ads_enabled', True)) }}
-
-select * 
-from {{ var('search_term_ad_keyword_report') }}
+{{
+    fivetran_utils.union_data(
+        table_identifier='search_term_ad_keyword_report', 
+        database_variable='amazon_ads_database', 
+        schema_variable='amazon_ads_schema', 
+        default_database=target.database,
+        default_schema='amazon_ads',
+        default_variable='search_term_ad_keyword_report_source',
+        union_schema_variable='amazon_ads_union_schemas',
+        union_database_variable='amazon_ads_union_databases'
+    )
+}}
